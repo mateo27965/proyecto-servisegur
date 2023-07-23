b7f94c4c9c85eaa1505034ef18a2f4fd26beac45
mateo27965 <20204327@aloe.ulima.edu.pe>
2023-07-22 21:49:32 -0500

    holaa

--- archivo.cpp
+++ archivo.cpp
@@ -1,9 +1,9 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
-
+using namespace std;
 int main() {
     // Write C++ code here
-    std::cout << "Hello world!";
+    cout << "Hello world!";
 
     return 0;
 }
